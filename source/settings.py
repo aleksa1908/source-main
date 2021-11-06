234f1c484342c24b785833cc9ad15295cae71a76
aleksa1908 <90071684+aleksa1908@users.noreply.github.com>
2021-11-06 02:42:14 +0100

    Update settings.py

--- source/settings.py
+++ source/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +128,7 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'shopsite/products/images')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
