984da79ed3da960bbf1b6f52476c778de4bc79be
aleksa1908 <90071684+aleksa1908@users.noreply.github.com>
2021-11-06 03:06:35 +0100

    Update settings.py

--- source/settings.py
+++ source/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-16lc)^$tru)zd1mg^^+@wj-+wgqczh#mch%=#(f888r)_av)lb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
